0aca7e3119691ffaa0e0ca460db6159de20e1c8e
上田智也 <tomo@uedatomoyanoMacBook-Pro.local>
2020-07-12 16:23:31 +0900

    5-9-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_up_path_for(resource)
-  # #   flash[:notice] = "Welcome! You have signed up successfully."
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+    flash[:notice] = "Welcome! You have signed up successfully."
+  end
 
   def after_sign_in_path_for(resource)
-    books_path # ログイン後に遷移するpathを設定
+    user_path(current_user.id) # ログイン後に遷移するpathを設定
     # flash[:notice] = "Signed in successfully."
   end
 
